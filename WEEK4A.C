912fdf2665e8f48e758b682f3e7505fbe7a0e240
adojan99 <128308829+adojan99@users.noreply.github.com>
2023-03-26 06:00:51 -0700

    Create WEEK4A.C

---
+++ WEEK4A.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n1, n2, mult;
+	printf("Enter two numbers:");
+	scanf_s("%d %d", &n1, &n2);
+	mult = n1 * n2;
+	printf("mult = %d", mult);
+	_getch();
+
+}
